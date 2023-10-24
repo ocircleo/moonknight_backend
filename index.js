e541611ebd1ae0e7d4da9cb1575d8fe4fd7a8d15
ocircleo <salmanhossain11222626@gmail.com>
2023-10-24 14:58:47 +0600

    server skeloton made

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 3000;
-const userRoute = require('./routes/userRoutes');
-//middlwire
+const userRoute = require("./routes/userRoutes");
+const moderatorRoute = require("./routes/moderatorRoutes");
+const adminRoute = require("./routes/adminRoutes");
+const path = require("path");
+//middleware
 app.use(express.json());
-require('dotenv').config()
+require("dotenv").config();
 
 //routes
+app.use("/user", userRoute);
+app.use("/moderator", moderatorRoute);
+app.use("/admin", adminRoute);
 
-app.use('/user',userRoute)
-
-app.use((req,res)=>{
-    res.send('<h1> 404! not a valid url</h1>')
-})
-app.listen(port,()=>{
-    console.log('app is runnign at port 3000')
-})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+// default route for error handling
+app.use((req, res) => {
+  res.status(404).send("<h1> 404! not a route</h1>");
+});
+app.listen(port, () => {
+  console.log(`app is running at port http://localhost:3000/ port ${port}`);
+});
